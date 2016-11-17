95c4b64d843ff7b11832f32ea9a8c6e23720611d
Jeroen van 't Hert <jeroen@yottabyte.io>
2016-11-17 14:06:57 +0100

    Fix unittests

--- tests/test_typeahead.py
+++ tests/test_typeahead.py
@@ -24,120 +24,232 @@ class TestTypeahead(unittest.TestCase):
         self.assertEqual(response.data, b'up and running',
                          'index page not quite as expected')
 
-    def test_api_typahead_get(self):
-        mock_json_content = \
-            b'[' \
-            b'{"content":[' \
-            b'{"_display":"Yottabyte Solutions",' \
-            b'"uri":"handelsregister/maatschappelijkeactiviteit/61130354/"},' \
-            b'{"_display":"Lotta",' \
-            b'"uri":"handelsregister/maatschappelijkeactiviteit/34341717/"},' \
-            b'{"_display":"Koninklijke Fabrieken Posthumus B.V.",' \
-            b'"uri":"handelsregister/maatschappelijkeactiviteit/33013145/"},' \
-            b'{"_display":"DOETA",' \
-            b'"uri":"handelsregister/maatschappelijkeactiviteit/65592719/"},' \
-            b'{"_display":"yolga",' \
-            b'"uri":"handelsregister/maatschappelijkeactiviteit/65431200/"}],' \
-            b'"label":"Maatschappelijke activiteiten"},' \
-            b'{"content":[' \
-            b'{"_display":"Yottabyte Solutions - Van Spilbergenstraat 95",' \
-            b'"uri":"handelsregister/vestiging/000030256488/"},' \
-            b'{"_display":"Lotta - Zamenhofstraat 110",' \
-            b' "uri":"handelsregister/vestiging/000008813965/"},' \
-            b'{"_display":"Yokata - Vrolikstraat 238",' \
-            b'"uri":"handelsregister/vestiging/000007095988/"},' \
-            b'{"_display":"Yogya - Johan Huizingalaan 142",' \
-            b'"uri":"handelsregister/vestiging/000007582978/"},' \
-            b'{"_display":"DOETA - Kampina 9",' \
-            b'"uri":"handelsregister/vestiging/000034303162/"},' \
-            b'{"_display":"Yokata - Lutmastraat 182",' \
-            b'"uri":"handelsregister/vestiging/000033381151/"},' \
-            b'{"_display":"Volta - Houtmankade 336",' \
-            b'"uri":"handelsregister/vestiging/000029763665/"},' \
-            b'{"_display":"yolga - Bernard Shawsingel 284",' \
-            b'"uri":"handelsregister/vestiging/000034156488/"},' \
-            b'{"_display":"ATTA - Von Zesenstraat 179",' \
-            b'"uri":"handelsregister/vestiging/000000343528/"},' \
-            b'{"_display":"Totta Research N.V. - Burgemeester Stramanweg 105",' \
-            b'"uri":"handelsregister/vestiging/000026200902/"}' \
-            b'],' \
-            b'"label":"Vestigingen"}' \
-            b']'
+    @staticmethod
+    def get_mock_json_content():
+        return bytes("""[
+  {
+    "content": [
+      {
+        "_display": "Yottabyte Solutions",
+        "uri": "handelsregister\/maatschappelijkeactiviteit\/61130354\/"
+      },
+      {
+        "_display": "Lotta",
+        "uri": "handelsregister\/maatschappelijkeactiviteit\/34341717\/"
+      },
+      {
+        "_display": "Koninklijke Fabrieken Posthumus B.V.",
+        "uri": "handelsregister\/maatschappelijkeactiviteit\/33013145\/"
+      },
+      {
+        "_display": "DOETA",
+        "uri": "handelsregister\/maatschappelijkeactiviteit\/65592719\/"
+      },
+      {
+        "_display": "yolga",
+        "uri": "handelsregister\/maatschappelijkeactiviteit\/65431200\/"
+      }
+    ],
+    "label": "Maatschappelijke activiteiten"
+  },
+  {
+    "content": [
+      {
+        "_display": "Yottabyte Solutions - Van Spilbergenstraat 95",
+        "uri": "handelsregister\/vestiging\/000030256488\/"
+      },
+      {
+        "_display": "Lotta - Zamenhofstraat 110",
+        "uri": "handelsregister\/vestiging\/000008813965\/"
+      },
+      {
+        "_display": "Yokata - Vrolikstraat 238",
+        "uri": "handelsregister\/vestiging\/000007095988\/"
+      },
+      {
+        "_display": "Yogya - Johan Huizingalaan 142",
+        "uri": "handelsregister\/vestiging\/000007582978\/"
+      },
+      {
+        "_display": "DOETA - Kampina 9",
+        "uri": "handelsregister\/vestiging\/000034303162\/"
+      },
+      {
+        "_display": "Yokata - Lutmastraat 182",
+        "uri": "handelsregister\/vestiging\/000033381151\/"
+      },
+      {
+        "_display": "Volta - Houtmankade 336",
+        "uri": "handelsregister\/vestiging\/000029763665\/"
+      },
+      {
+        "_display": "yolga - Bernard Shawsingel 284",
+        "uri": "handelsregister\/vestiging\/000034156488\/"
+      },
+      {
+        "_display": "ATTA - Von Zesenstraat 179",
+        "uri": "handelsregister\/vestiging\/000000343528\/"
+      },
+      {
+        "_display": "Totta Research N.V. - Burgemeester Stramanweg 105",
+        "uri": "handelsregister\/vestiging\/000026200902\/"
+      }
+    ],
+    "label": "Vestigingen"
+  }
+]
+""", 'utf-8')
 
-        expected_response = \
-            b'[' \
-            b'{"content": [' \
-            b'{"_display": "Yottabyte Solutions", ' \
-            b'"uri": "handelsregister/maatschappelijkeactiviteit/61130354/"}, ' \
-            b'{"_display": "Lotta", ' \
-            b'"uri": "handelsregister/maatschappelijkeactiviteit/34341717/"}, ' \
-            b'{"_display": "Koninklijke Fabrieken Posthumus B.V.", ' \
-            b'"uri": "handelsregister/maatschappelijkeactiviteit/33013145/"}, ' \
-            b'{"_display": "DOETA", ' \
-            b'"uri": "handelsregister/maatschappelijkeactiviteit/65592719/"}, ' \
-            b'{"_display": "yolga", ' \
-            b'"uri": "handelsregister/maatschappelijkeactiviteit/65431200/"}' \
-            b'], ' \
-            b'"label": "Maatschappelijke activiteiten"}, ' \
-            b'{"content": [' \
-            b'{"_display": "Yottabyte Solutions - Van Spilbergenstraat 95", ' \
-            b'"uri": "handelsregister/vestiging/000030256488/"}, ' \
-            b'{"_display": "Lotta - Zamenhofstraat 110", ' \
-            b'"uri": "handelsregister/vestiging/000008813965/"}, ' \
-            b'{"_display": "Yokata - Vrolikstraat 238", ' \
-            b'"uri": "handelsregister/vestiging/000007095988/"}, ' \
-            b'{"_display": "Yogya - Johan Huizingalaan 142", ' \
-            b'"uri": "handelsregister/vestiging/000007582978/"}, ' \
-            b'{"_display": "DOETA - Kampina 9", ' \
-            b'"uri": "handelsregister/vestiging/000034303162/"}, ' \
-            b'{"_display": "Yokata - Lutmastraat 182", ' \
-            b'"uri": "handelsregister/vestiging/000033381151/"}, ' \
-            b'{"_display": "Volta - Houtmankade 336", ' \
-            b'"uri": "handelsregister/vestiging/000029763665/"}, ' \
-            b'{"_display": "yolga - Bernard Shawsingel 284", ' \
-            b'"uri": "handelsregister/vestiging/000034156488/"}, ' \
-            b'{"_display": "ATTA - Von Zesenstraat 179", ' \
-            b'"uri": "handelsregister/vestiging/000000343528/"}, ' \
-            b'{"_display": "Totta Research N.V. - Burgemeester Stramanweg 105", ' \
-            b'"uri": "handelsregister/vestiging/000026200902/"}], ' \
-            b'"label": "Vestigingen"}, ' \
-            b'{"content": [' \
-            b'{"_display": "Yottabyte Solutions", ' \
-            b'"uri": "handelsregister/maatschappelijkeactiviteit/61130354/"}, ' \
-            b'{"_display": "Lotta", ' \
-            b'"uri": "handelsregister/maatschappelijkeactiviteit/34341717/"}, ' \
-            b'{"_display": "Koninklijke Fabrieken Posthumus B.V.", ' \
-            b'"uri": "handelsregister/maatschappelijkeactiviteit/33013145/"}, ' \
-            b'{"_display": "DOETA", ' \
-            b'"uri": "handelsregister/maatschappelijkeactiviteit/65592719/"},' \
-            b' {"_display": "yolga", ' \
-            b'"uri": "handelsregister/maatschappelijkeactiviteit/65431200/"}' \
-            b'], ' \
-            b'"label": "Maatschappelijke activiteiten"}, ' \
-            b'{"content": [' \
-            b'{"_display": "Yottabyte Solutions - Van Spilbergenstraat 95", ' \
-            b'"uri": "handelsregister/vestiging/000030256488/"}, ' \
-            b'{"_display": "Lotta - Zamenhofstraat 110", ' \
-            b'"uri": "handelsregister/vestiging/000008813965/"}, ' \
-            b'{"_display": "Yokata - Vrolikstraat 238", ' \
-            b'"uri": "handelsregister/vestiging/000007095988/"}, ' \
-            b'{"_display": "Yogya - Johan Huizingalaan 142", ' \
-            b'"uri": "handelsregister/vestiging/000007582978/"}, ' \
-            b'{"_display": "DOETA - Kampina 9", ' \
-            b'"uri": "handelsregister/vestiging/000034303162/"}, ' \
-            b'{"_display": "Yokata - Lutmastraat 182", ' \
-            b'"uri": "handelsregister/vestiging/000033381151/"}, ' \
-            b'{"_display": "Volta - Houtmankade 336", ' \
-            b'"uri": "handelsregister/vestiging/000029763665/"}, ' \
-            b'{"_display": "yolga - Bernard Shawsingel 284", ' \
-            b'"uri": "handelsregister/vestiging/000034156488/"}, ' \
-            b'{"_display": "ATTA - Von Zesenstraat 179", ' \
-            b'"uri": "handelsregister/vestiging/000000343528/"}, ' \
-            b'{"_display": "Totta Research N.V. - Burgemeester Stramanweg 105", ' \
-            b'"uri": "handelsregister/vestiging/000026200902/"}' \
-            b'], ' \
-            b'"label": "Vestigingen"}' \
-            b']\n'
+    @staticmethod
+    def get_expected_response():
+        return json.loads("""
+[
+  {
+    "content": [
+      {
+        "_display": "Yottabyte Solutions",
+        "uri": "handelsregister\/maatschappelijkeactiviteit\/61130354\/"
+      },
+      {
+        "_display": "Lotta",
+        "uri": "handelsregister\/maatschappelijkeactiviteit\/34341717\/"
+      },
+      {
+        "_display": "Koninklijke Fabrieken Posthumus B.V.",
+        "uri": "handelsregister\/maatschappelijkeactiviteit\/33013145\/"
+      },
+      {
+        "_display": "DOETA",
+        "uri": "handelsregister\/maatschappelijkeactiviteit\/65592719\/"
+      },
+      {
+        "_display": "yolga",
+        "uri": "handelsregister\/maatschappelijkeactiviteit\/65431200\/"
+      }
+    ],
+    "label": "Maatschappelijke activiteiten"
+  },
+  {
+    "content": [
+      {
+        "_display": "Yottabyte Solutions - Van Spilbergenstraat 95",
+        "uri": "handelsregister\/vestiging\/000030256488\/"
+      },
+      {
+        "_display": "Lotta - Zamenhofstraat 110",
+        "uri": "handelsregister\/vestiging\/000008813965\/"
+      },
+      {
+        "_display": "Yokata - Vrolikstraat 238",
+        "uri": "handelsregister\/vestiging\/000007095988\/"
+      },
+      {
+        "_display": "Yogya - Johan Huizingalaan 142",
+        "uri": "handelsregister\/vestiging\/000007582978\/"
+      },
+      {
+        "_display": "DOETA - Kampina 9",
+        "uri": "handelsregister\/vestiging\/000034303162\/"
+      },
+      {
+        "_display": "Yokata - Lutmastraat 182",
+        "uri": "handelsregister\/vestiging\/000033381151\/"
+      },
+      {
+        "_display": "Volta - Houtmankade 336",
+        "uri": "handelsregister\/vestiging\/000029763665\/"
+      },
+      {
+        "_display": "yolga - Bernard Shawsingel 284",
+        "uri": "handelsregister\/vestiging\/000034156488\/"
+      },
+      {
+        "_display": "ATTA - Von Zesenstraat 179",
+        "uri": "handelsregister\/vestiging\/000000343528\/"
+      },
+      {
+        "_display": "Totta Research N.V. - Burgemeester Stramanweg 105",
+        "uri": "handelsregister\/vestiging\/000026200902\/"
+      }
+    ],
+    "label": "Vestigingen"
+  },
+  {
+    "content": [
+      {
+        "_display": "Yottabyte Solutions",
+        "uri": "handelsregister\/maatschappelijkeactiviteit\/61130354\/"
+      },
+      {
+        "_display": "Lotta",
+        "uri": "handelsregister\/maatschappelijkeactiviteit\/34341717\/"
+      },
+      {
+        "_display": "Koninklijke Fabrieken Posthumus B.V.",
+        "uri": "handelsregister\/maatschappelijkeactiviteit\/33013145\/"
+      },
+      {
+        "_display": "DOETA",
+        "uri": "handelsregister\/maatschappelijkeactiviteit\/65592719\/"
+      },
+      {
+        "_display": "yolga",
+        "uri": "handelsregister\/maatschappelijkeactiviteit\/65431200\/"
+      }
+    ],
+    "label": "Maatschappelijke activiteiten"
+  },
+  {
+    "content": [
+      {
+        "_display": "Yottabyte Solutions - Van Spilbergenstraat 95",
+        "uri": "handelsregister\/vestiging\/000030256488\/"
+      },
+      {
+        "_display": "Lotta - Zamenhofstraat 110",
+        "uri": "handelsregister\/vestiging\/000008813965\/"
+      },
+      {
+        "_display": "Yokata - Vrolikstraat 238",
+        "uri": "handelsregister\/vestiging\/000007095988\/"
+      },
+      {
+        "_display": "Yogya - Johan Huizingalaan 142",
+        "uri": "handelsregister\/vestiging\/000007582978\/"
+      },
+      {
+        "_display": "DOETA - Kampina 9",
+        "uri": "handelsregister\/vestiging\/000034303162\/"
+      },
+      {
+        "_display": "Yokata - Lutmastraat 182",
+        "uri": "handelsregister\/vestiging\/000033381151\/"
+      },
+      {
+        "_display": "Volta - Houtmankade 336",
+        "uri": "handelsregister\/vestiging\/000029763665\/"
+      },
+      {
+        "_display": "yolga - Bernard Shawsingel 284",
+        "uri": "handelsregister\/vestiging\/000034156488\/"
+      },
+      {
+        "_display": "ATTA - Von Zesenstraat 179",
+        "uri": "handelsregister\/vestiging\/000000343528\/"
+      },
+      {
+        "_display": "Totta Research N.V. - Burgemeester Stramanweg 105",
+        "uri": "handelsregister\/vestiging\/000026200902\/"
+      }
+    ],
+    "label": "Vestigingen"
+  }
+]
+""")
+
+    def test_api_typahead_get(self):
+        mock_json_content = self.get_mock_json_content()
+        expected_response = self.get_expected_response()
 
         mock_hr_response = MockResponse(
             encoding='utf-8',
@@ -146,37 +258,68 @@ class TestTypeahead(unittest.TestCase):
             url='http://hr.endpoint.internal',
             json_data=mock_json_content)
 
-        when(sessions.Session).request(
-            'GET',
-            'http://hr.endpoint.internal',
-            timeout=0.5,
-            stream=False,
-            parameters={'q': 'gibberish get'}).thenReturn(mock_hr_response)
+        try:
+            when(sessions.Session).request(
+                'GET',
+                'http://hr.endpoint.internal',
+                timeout=0.5,
+                stream=False,
+                parameters={'q': 'gibberish get'}).thenReturn(mock_hr_response)
 
-        when(sessions.Session).request(
-            'GET',
-            'http://nap.endpoint.internal',
-            timeout=0.5,
-            stream=False,
-            parameters={'q': 'gibberish get'}).thenReturn(mock_hr_response)
+            when(sessions.Session).request(
+                'GET',
+                'http://nap.endpoint.internal',
+                timeout=0.5,
+                stream=False,
+                parameters={'q': 'gibberish get'}).thenReturn(mock_hr_response)
 
-        response = self.app.get('/tr?q=gibberish%20get')
+            response = self.app.get('/tr?q=gibberish%20get')
 
-        self.assertEqual(response.status_code, 200, 'api not working')
+            self.assertEqual(response.status_code, 200, 'api not working')
 
-        self.assertEqual(response.data, expected_response,
-                         'api response not quite as expected')
-        self.assertEqual(response.content_type, 'application/json',
-                         'Verkeerd response type')
+            self.assertEqual(json.loads(response.data.decode('utf-8')),
+                             expected_response,
+                             'api response not quite as expected')
+            self.assertEqual(response.content_type, 'application/json',
+                             'Verkeerd response type')
+        finally:
+            unstub()
 
     def test_api_typahead_post(self):
-        response = self.app.post('/tr', data={'q': 'gibberish post'})
-        self.assertEqual(response.status_code, 200, 'api status: OK')
-        self.assertEqual(response.data,
-                         b'{"get": "you send: gibberish post"}\n',
-                         'api response not quite as expected')
-        self.assertEqual(response.content_type, 'application/json',
-                         'Verkeerd response type')
+        mock_json_content = self.get_mock_json_content()
+        expected_response = self.get_expected_response()
+
+        mock_hr_response = MockResponse(
+            encoding='utf-8',
+            status_code=200,
+            ok=True,
+            url='http://hr.endpoint.internal',
+            json_data=mock_json_content)
+
+        try:
+            when(sessions.Session).request(
+                'GET',
+                'http://hr.endpoint.internal',
+                timeout=0.5,
+                stream=False,
+                parameters={'q': 'gibberish post'}).thenReturn(mock_hr_response)
+
+            when(sessions.Session).request(
+                'GET',
+                'http://nap.endpoint.internal',
+                timeout=0.5,
+                stream=False,
+                parameters={'q': 'gibberish post'}).thenReturn(mock_hr_response)
+
+            response = self.app.post('/tr', data={'q': 'gibberish post'})
+            self.assertEqual(response.status_code, 200, 'api status: OK')
+            self.assertEqual(json.loads(response.data.decode('utf-8')),
+                             expected_response,
+                             'api response not quite as expected')
+            self.assertEqual(response.content_type, 'application/json',
+                             'Verkeerd response type')
+        finally:
+            unstub()
 
     def test_str(self):
         sut = TypeAheadResponses([

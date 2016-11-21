37c0d1b3535b252a3bb1f90a6834c11fbddeaea7
Jeroen van 't Hert <jeroen@yottabyte.io>
2016-11-21 16:52:58 +0100

    Config

--- typeahead/conf.py
+++ typeahead/conf.py
@@ -20,17 +20,18 @@ DEBUG = True
 DEFAULT_UPSTREAM_TIMEOUT = 0.5
 
 UPSTREAM_CONFIG = {
+    'bag': {
+        'endpoint': 'http://bag-api.service.consul:8096/atlas/typeahead/',
+        'maxresults': 5,
+        'weight': 6,
+        'timeout': DEFAULT_UPSTREAM_TIMEOUT
+    },
     'hr': {
-        'endpoint': 'https://api-acc.datapunt.amsterdam.nl/handelsregister/typeahead',
+        'endpoint': 'http://handelsregister-api.service.consul:8101/handelsregister/typeahead',
         'maxresults': 5,
         'weight': 10,
         'timeout': DEFAULT_UPSTREAM_TIMEOUT
 
-    },
-    'bag': {
-        'endpoint': 'https://api-acc.datapunt.amsterdam.nl/atlas/typeahead/',
-        'maxresults': 5,
-        'weight': 6,
-        'timeout': DEFAULT_UPSTREAM_TIMEOUT
     }
 }
+

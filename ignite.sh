f872eaab6eb0889ddf96bbd0223944c4c70073c2
Jeroen van 't Hert <jeroen@yottabyte.io>
2017-01-04 11:14:25 +0100

    Try to get external ip of docker host.

--- ignite.sh
+++ ignite.sh
@@ -5,6 +5,18 @@ set -u
 
 echo '' | sudo echo "bypassing the lecture"
 
+METADATA_HOST=169.254.169.254
+METADATA_PORT=80
+METADATA_URL=http://${METADATA_HOST}:${METADATA_PORT}/latest/meta-data/local-ipv4
+
 export DOCKER_IP=$(hostname --ip-address)
+
+#Check connectivity
+curl -s -m1 ${METADATA_URL} || exitcode=$?
+if [ "${exitcode}" -ne 0 ]; then
+  export DOCKER_EXTERNAL_IP=${DOCKER_IP}
+else
+  export DOCKER_EXTERNAL_IP=$(curl ${METADATA_URL})
+fi
 
 uwsgi --ini /app/uwsgi.ini
